a0bfbf571b473b9e720f4a34a85bf577c15351b8
Sam Ezeh <sam.z.ezeh@gmail.com>
2022-07-21 20:17:57 +0100

    Add testing boilerplate

---
+++ test/__init__.py
@@ -0,0 +1,5 @@
+from sirenity import __main__
+
+def test_main():
+    # Should not error
+    __main__.main()
---
+++ test/test_main.py
@@ -0,0 +1,6 @@
+from sirenity import __main__
+
+def test_main():
+    # Should not error
+    __main__.main()
+
