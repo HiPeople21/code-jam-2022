8ea262e88931509dafd0e2f719c28d2addb5bf63
Yubo Wang <yubowang2007@gmail.com>
2022-07-23 20:25:27 +0100

    Fixing tests #2

--- tests/test_problems.py
+++ tests/test_problems.py
@@ -6,6 +6,8 @@ from sirenity.euler import ProblemManager
 from sirenity.euler.problem import Problem
 from sirenity.euler.problems_manager import NotFoundError
 
+unittest.TestLoader.sortTestMethodsUsing = None
+
 
 class TestProblemManager(unittest.TestCase):
     """Tests ProblemManager"""
@@ -22,19 +24,19 @@ class TestProblemManager(unittest.TestCase):
             f.write("")
 
         self.manager = ProblemManager("test.db")
-        self.manager.create_table()
 
-    def test_create_table(self) -> None:
+    def create_table(self) -> None:
         """Tests that table is created"""
         con = sqlite3.connect(self.manager._file)
         cur = con.cursor()
+        self.manager.create_table()
         cur.execute(
             "SELECT name FROM sqlite_master WHERE type='table' AND name='problems';"
         )
         self.assertEqual(len(cur.fetchall()), 1)
         con.close()
 
-    def test_add_and_get_problem(self) -> None:
+    def add_and_get_problem(self) -> None:
         """Tests that problems are added and are retrieved"""
         self.manager.add_to_db(prompt="Problem prompt", solution="solution")
         problem = self.manager.get_at_id(1)
@@ -42,12 +44,12 @@ class TestProblemManager(unittest.TestCase):
         self.assertEqual(problem.prompt, "Problem prompt")
         self.assertEqual(problem.solution, "solution")
 
-    def test_get_error(self) -> None:
+    def get_error(self) -> None:
         """Tests that error is thrown if id is out of range"""
         with self.assertRaises(NotFoundError):
             self.manager.get_at_id(2)
 
-    def test_check_solution(self) -> None:
+    def check_solution(self) -> None:
         """
         Tests that the class returns the right boolean
         depending on if the solution is correct or not
@@ -55,9 +57,16 @@ class TestProblemManager(unittest.TestCase):
         self.assertTrue(self.manager.check_solution("solution", 1))
         self.assertFalse(self.manager.check_solution("20933", 1))
 
-    def test_check_random_problem(self) -> None:
+    def check_random_problem(self) -> None:
         """Checks that a random problem is returned"""
         self.assertTrue(isinstance(self.manager.get_random_problem(), Problem))
+
+    def test_all(self):
+        self.create_table()
+        self.add_and_get_problem()
+        self.get_error()
+        self.check_solution()
+        self.check_random_problem()
 
 
 if __name__ == "__main__":
