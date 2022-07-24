46fc04dbc50aa8f0f026748752edde3d8af097af
Astrogene <74155671+Astrogene@users.noreply.github.com>
2022-07-24 13:21:20 +0000

    Create a frontend UI (#24)
    * Create css files
    
    * Add button to start a game
    
    * Add heading for the front page
    
    * Add tailwind.css to the index.html file
    
    * Created new game page
    
    * Added create.html for creating games.
    
    * Added button for creating game.
    
    * Format code using black
    
    Co-authored-by: Sam Ezeh <sam.z.ezeh@gmail.com>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- sirenity/app.py
+++ sirenity/app.py
@@ -12,3 +12,9 @@ templates = Jinja2Templates(directory="templates")
 def index(request: Request):
     """The main site page"""
     return templates.TemplateResponse("index.html", {"request": request})
+
+
+@app.get("/create")
+def game(request: Request):
+    """The game creation form"""
+    return templates.TemplateResponse("create.html", {"request": request})
