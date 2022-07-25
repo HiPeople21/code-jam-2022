8cd7605ab60018bc36471df90f57388abc0810fa
Sam Ezeh <sam.z.ezeh@gmail.com>
2022-07-25 08:33:03 +0100

    Make the app path independent (#23)
    * Move static directory inside the sirenity module
    
    * Move template directory inside the sirenity module
    
    * Make references to the template and static directory path independent

--- sirenity/app.py
+++ sirenity/app.py
@@ -1,11 +1,15 @@
+import pathlib
+
 from fastapi import FastAPI, Request
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
+ROOT = pathlib.Path(__file__).parent
+
 app = FastAPI()
 
-app.mount("/static", StaticFiles(directory="static"), name="static")
-templates = Jinja2Templates(directory="templates")
+app.mount("/static", StaticFiles(directory=ROOT / "static"), name="static")
+templates = Jinja2Templates(directory=ROOT / "templates")
 
 
 @app.get("/")
